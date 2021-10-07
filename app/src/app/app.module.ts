b9e90359152598bc26ef7dac92e1b78fb522bdd5
mariano <mariano.juarez2604@gmail.com>
2021-10-07 03:15:57 -0300

    p

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { LamparaComponent } from './lamparas/lampara/lampara.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CarritoComponent } from './carrito/carrito.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { CarritoComponent } from './carrito/carrito.component';
     LamparasComponent,
     PresentacionComponent,
     LamparaComponent,
-    CarritoComponent
+    CarritoComponent,
+    NavbarComponent,
+    EncabezadoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
